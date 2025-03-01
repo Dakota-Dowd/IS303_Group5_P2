fc2fa228ae63d16207ba8c2a9d4a69010bee070d
Dakota-Dowd <Jmdowd2020@gmail.com>
2025-02-28 17:21:50 -0700

    Expanded Intro Message

--- Introduction_Function.py
+++ Introduction_Function.py
@@ -7,6 +7,7 @@
 def Player(sName):
     # Displaying the introduction to the game
     print("\nHello " + str(sName) + "!")
-    print("\nWelcome to the Soccer Tournament! \n\nThis is a game to generate random scores for a women's soccer tournament. \n\nPlease enter information as you are prompted, and we will discover the results of the tournament together!")
+    print("\nWelcome to the NCAA Women's Soccer Tournament! \n\nThis is a game to generate random scores for a women's soccer tournament. \n\nFirst, we will ask you the name of the home team, as well as how many games that team will play.")
+    print("\nThen, based on how many games are played, you will be asked to name opposing teams. \n\nIn the end, we will generate scores and show you how well your team did during the season! Let's go!")
     return (sName)
 

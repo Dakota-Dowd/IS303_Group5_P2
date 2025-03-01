63564a5206d8616ffbb1cbf8adae7f11d93cd871
Joshknight13 <166788639+Joshknight13@users.noreply.github.com>
2025-03-01 15:56:49 -0700

    added in the base of stuff

--- P2_Project.py
+++ P2_Project.py
@@ -1,8 +1,23 @@
 # Caleb Caten, Dakota Dowd, Isaac Pratte, Josh Knight Lincoln Adams
 
-# Imports Custum Functions
+# Imports Custom Functions
 from Introduction_Function import Player
+from josh_funct5 import displayRecord
+from Caleb_menu_function import menu
 
 # Welcome: Requests the user's name and displays an introductory message
 sName = input("What is your name? ")
 sName = Player(sName)
+
+option = 0
+menu()
+
+if option == 1 :
+    #function 3
+
+if option == 2 :
+
+if option == 3 :
+    displayRecord(wins, losses, sHomeTeam)
+if option == 4 :
+    print("Exiting the game, see you next time!")

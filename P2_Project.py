70e6c0645575c68efa73221c3ad7f1d6a3c849a7
Joshknight13 <166788639+Joshknight13@users.noreply.github.com>
2025-03-01 16:31:20 -0700

    Added a win loss counter

--- P2_Project.py
+++ P2_Project.py
@@ -4,20 +4,31 @@
 from Introduction_Function import Player
 from josh_funct5 import displayRecord
 from Caleb_menu_function import menu
+from function4 import playGame
 
 # Welcome: Requests the user's name and displays an introductory message
 sName = input("What is your name? ")
 sName = Player(sName)
 
 option = 0
+wins = 0
+losses = 0
 menu()
 
 if option == 1 :
     #function 3
 
 if option == 2 :
+    bOutcome = playGame("josh", "Knight")
+
+    #create an if statement to keep track of wins and losses
+    if bOutcome == True :
+        wins = wins + 1
+    if bOutcome == False :
+        losses = losses + 1
 
 if option == 3 :
     displayRecord(wins, losses, sHomeTeam)
+
 if option == 4 :
     print("Exiting the game, see you next time!")

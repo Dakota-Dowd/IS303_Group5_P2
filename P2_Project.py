1a705861483c0137bf6b7455e1f38c07d7e8b2e5
Dakota-Dowd <Jmdowd2020@gmail.com>
2025-02-28 17:27:59 -0700

    Welcome Comments

--- P2_Project.py
+++ P2_Project.py
@@ -1,6 +1,8 @@
 # Caleb Caten, Dakota Dowd, Isaac Pratte, Josh Knight Lincoln Adams
 
+# Imports Custum Functions
 from Introduction_Function import Player
 
+# Welcome: Requests the user's name and displays an introductory message
 sName = input("What is your name? ")
 sName = Player(sName)
